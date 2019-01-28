64530f4185bcb40cc95b4979779c3d32277fa724
michael.ohalloran@polarisalpha.com <michael.ohalloran@polarisalpha.com>
2019-01-28 13:13:01 -0700

    added observable search, commit before adding reactive form

--- src/app/music-search/music-search.component.ts
+++ src/app/music-search/music-search.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchService } from '../search.service';
+import { Song } from '../song.model';
+import { Observable } from 'rxjs';
+import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'music-search',
@@ -10,10 +14,13 @@ export class MusicSearchComponent implements OnInit {
 
   songs: any[] = [];
   private loading: boolean = false;
+  private results: Observable<Song[]>;
+  private searchField: FormControl;
 
   constructor(private itunes: SearchService) {}
 
   ngOnInit() {
+    this.searchField = new FormControl();
     this.getSongs();
     console.log('ran onInit getSongs');
     this.itunes.songsChanged.subscribe(
@@ -28,11 +35,23 @@ export class MusicSearchComponent implements OnInit {
     this.songs = this.itunes.results;
   }
 
+  // onSearch(term: string) {
+  //   console.log(`You searched for ${term}`);
+  //   this.loading = true;
+  //   this.itunes.search(term)
+  //     .then(()=> this.loading = false);
+  // }
+
   onSearch(term: string) {
     console.log(`You searched for ${term}`);
     this.loading = true;
     this.itunes.search(term)
-      .then(()=> this.loading = false);
+      .subscribe(
+        data => {
+          this.loading = false;
+          this.results = this.itunes.search(term);
+        }
+      )
   }
 
 }
--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, Output, EventEmitter } from '@angular/core';
 import { Http, HttpModule, Response } from '@angular/http';
 import { Song } from './song.model';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -37,37 +39,59 @@ export class SearchService {
   // }
 
 
-  search(term: string) {
-    let apiURL = `${this.apiRoot}?term=${term}&media=music&limit=20`;
-    let promise = new Promise((resolve, reject) => {
-      this.http.get(apiURL)
-        .toPromise()
-        .then(res => {
-          this.results = res.json().results.map(song => {
-            let time = this.formatTime(song.trackTimeMillis/1000);
-            return new Song(
-              song.trackName,
-              song.collectionName,
-              song.artistName,
-              song.artworkUrl100,
-              time
-            )
-          });
-          console.log('results: ', this.results);
-          resolve();
-          this.songsChanged.emit(this.results);
-          return this.results;
-        },
-        errMsg => {
-          reject();
-        })
-        // .then(data => {
-        //   this.curatedResults = this.makeSongDivs(data);
-        //   console.log('curated: ', this.curatedResults);
-        //   this.songsChanged.emit(this.curatedResults);
-        // },
-    });
-    return promise;
+  // search(term: string) {
+  //   let apiURL = `${this.apiRoot}?term=${term}&media=music&limit=20`;
+  //   let promise = new Promise((resolve, reject) => {
+  //     this.http.get(apiURL)
+  //       .toPromise()
+  //       .then(res => {
+  //         this.results = res.json().results.map(song => {
+  //           let time = this.formatTime(song.trackTimeMillis/1000);
+  //           return new Song(
+  //             song.trackName,
+  //             song.collectionName,
+  //             song.artistName,
+  //             song.artworkUrl100,
+  //             time
+  //           )
+  //         });
+  //         console.log('results: ', this.results);
+  //         resolve();
+  //         this.songsChanged.emit(this.results);
+  //         return this.results;
+  //       },
+  //       errMsg => {
+  //         reject();
+  //       })
+  //       // .then(data => {
+  //       //   this.curatedResults = this.makeSongDivs(data);
+  //       //   console.log('curated: ', this.curatedResults);
+  //       //   this.songsChanged.emit(this.curatedResults);
+  //       // },
+  //   });
+  //   return promise;
+  // }
+
+  search(term: string): Observable<Song[]> {
+    let apiURL = `${this.apiRoot}?term=${term}&media=music&limit=10`;
+      return this.http.get(apiURL)
+        .pipe(
+          map(res => {
+            console.log('res json: ', res.json());
+            this.results = res.json().results.map(item => {
+              let time = this.formatTime(item.trackTimeMillis/1000);
+              return new Song(
+                item.trackName,
+                item.collectionName,
+                item.artistName,
+                item.artworkUrl100,
+                time
+              )
+            });
+            this.songsChanged.emit(this.results);
+            return this.results;
+          })
+        )
   }
 
 }
